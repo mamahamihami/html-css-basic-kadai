08be60f04bd1c39a6e3418cc0f07a8dc94a579f7
mamahamihami <145332624+mamahamihami@users.noreply.github.com>
2024-03-31 18:38:44 +0900

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('#change-color').on('click', function () {
+        $('#target').css('color','red');
+    });
+    $('#change-text').on('click', function () {
+        $('#target').text('Hello！');
+    });
+    $('#fade-out').on('click', function () {
+        $('#target').css('opacity', 0);
+    })
+    $('#fade-in').on('click', function () {
+        $('#target').css('opacity', 1);
+    })
+
+})
